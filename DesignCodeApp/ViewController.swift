d858ea7137d175c775c1fc1b79b31ac71144d705
Orlando <commandzer01@gmail.com>
2018-07-27 16:51:33 -0500

    Initial Commit

---
+++ DesignCodeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DesignCodeApp
+//
+//  Created by Orlando Villarreal on 7/27/18.
+//  Copyright © 2018 Orlando Villarreal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
